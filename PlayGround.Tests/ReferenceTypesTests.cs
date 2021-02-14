9e4d45190f6569f2598a69e14b6bd847ec019b5e
JksHub <jks0101@hotmail.com>
2021-02-14 14:02:03 -0500

    Add new collection tests

--- PlayGround.Tests/ReferenceTypesTests.cs
+++ PlayGround.Tests/ReferenceTypesTests.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace PlayGround.Tests
@@ -22,9 +21,41 @@ namespace PlayGround.Tests
         {
             var collection = new[] { 3, 4, 5 };
 
+            var similarCollection = new[] { 3, 4, 5 };
+
             WipeArray(collection);
 
             Assert.NotEmpty(collection);
+            Assert.False(collection == similarCollection);
+        }
+
+        [Fact]
+        public void ComparingDifferentCollections()
+        {
+            var list = new List<int> { 3, 4, 5 };
+            var similarList = new List<int> { 3, 4, 5 };
+
+            var collection = new[] { 3, 4, 5 };
+            var similarCollection = new[] { 3, 4, 5 };
+
+
+            Assert.True(list.SequenceEqual(similarList));
+            Assert.True(collection.SequenceEqual(similarCollection));
+            Assert.True(list.SequenceEqual(similarCollection));
+        }
+
+        [Fact]
+        public void AssignmentCollectionsAreEquals()
+        {
+            var list = new List<int> { 3, 4, 5 };
+            var assignmentList = list;
+
+            var collection = new[] { 3, 4, 5 };
+            var assignmentCollection = collection;
+
+
+            Assert.True(list == assignmentList);
+            Assert.True(collection == assignmentCollection);
         }
 
         private void WipeArray(int[] collection) => collection = new int[] { };
